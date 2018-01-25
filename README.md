b09c1ea294156013b5be3d291197173db00cb2e8
JoshuaMathias <emathias@patas.ling.washington.edu>
2018-01-24 19:49:38 -0800

    Comments, commenting out unused string assignment, readme

--- README.md
+++ README.md
@@ -2,20 +2,18 @@
 
 Joshua Mathias & Kekoa Riggin - LING 571 - January 24, 2018
 
-## Problems
-
-
-## Solutions
-
+## Problems & Solutions
+Problem: Checking whether a symbol is a terminal.
+Solution: One option is checking for ' surrounding the symbol. We decided to use nltk's function is_terminal() to be sure to cover all cases (such as double quotes).
 
 ## Insights
-
+Looping efficiently through cells: For simplicity, our implementation checks whether cells are empty instead of not looping through them in the first place. We could use a stack or queue to know where to start/end each inner loop, to speed up the algorithm.
 
 ## Special Features
-
+We check at the beginning whether the input grammar is in Chomsky Normal Form and output a warning if it's not.
 
 ## Learned
-
+We learned how an converting a CFG to a binary form allows efficient parsing using dynamic programming. Dealing with subtrees and grammars is a good way to conceptualize the main idea behind dynamic programming, which is to reuse previous results/paths and do as little checking as possible while progressively moving through the search space.
 
 ## Division of Labor
 
--- hw3_parser.py
+++ hw3_parser.py
@@ -25,41 +25,42 @@ if not inputCFG.is_chomsky_normal_form():
 
 start = str(inputCFG.start())
 
-term = {} # dictionary of termanal symbols
+term = {} # dictionary of terminal symbols
 termInd = []
 nonTerm = {} # dictionary of nonterminal symbols
 nonTermInd = []
 
 rulecount = 0
-for inLine in inputCFG.productions(): # use grammar from nltk to auto handle  rules with '|'
+for inLine in inputCFG.productions(): # use grammar from nltk to auto handle rules with '|'
 	rule = str(inLine) # GET A STRING OF THE RULE
 	# print(rule)
 	pieces = rule.split()
+	if pieces[-1] == '': # In case the input has an extra space at the end.
+		pieces = pieces[:-1]
 	lhs = pieces[0] # lefthand side of rule
 	rhs = "" # righthand side of rule
-	if pieces[-1] == '':
-		pieces = pieces[:-1]
 
 	termCheck = False # check if rule is terminal
-	if len(pieces) > 3: # check if rule is binary
+	if len(pieces) > 3: # is rule binary?
 		rhs = pieces[2] + " " + pieces[3]
 	else:
-		if pieces[2][0] == "\'" and pieces[2][-1] == "\'": # check for terminals
+		if nltk.grammar.is_terminal(inLine.rhs()): # check for terminals
 			termCheck = True
 		rhs = pieces[2]
 
+	# Populate lists of terminals and nonterminals with all left hand sides from the rules for each terminal or nonterminal.
 	if termCheck == True:
 		if rhs in term:
 			termInd[term[rhs]].append(lhs)
 		else:
-			term[rhs] = len(termInd)
+			term[rhs] = len(termInd) # Assign terminal the next index.
 			termInd.append([lhs])
 	else:
 		if rhs in nonTerm:
 			nonTermInd[nonTerm[rhs]].append(lhs)
 
 		else:
-			nonTerm[rhs] = len(nonTermInd)
+			nonTerm[rhs] = len(nonTermInd)  # Assign nonterminal the next index.
 			nonTermInd.append([lhs])
 
 # --------------------
@@ -80,46 +81,50 @@ for sentence in sentences:
 		words[m] = "\'" + word + "\'"
 		m+=1
 	# Embedded array for Row, Column, Tree, [symbol, tree text]
+	# (n+1) x (n+1) matrix
 	neo = [ [ [None] for x in range(len(words)+1)] for y in range(len(words)+1)] # neo because the Matrix
 
 	numParses = 0
 	treeStr = ""
-	c = 1
+	c = 1 # column
 	while c < len(neo):
 		r = c - 1
 		addFirst = False # Check that diagonal cells replace Null and add all
 
+		# First row
+		# For each terminal, populate triangular matrix with its path, represented by the nonterminal pointing to it and the terminal symbol.
+		# r represents index of words in sentence (as well as the row)
 		for symbol in termInd[term[words[r]]]:
-			pathStr = "(" + symbol + " " + words[r] + ")"
-			if addFirst == False:
+			pathStr = "(" + symbol + " " + words[r] + ")" # Since these are terminals, we know that these are the innermost parentheses/path.
+			if addFirst == False: # The first time, initialize with a list.
 				neo[r][c] = [[symbol, pathStr]]
 				addFirst = True
 			else:
 				neo[r][c].append([symbol, pathStr])
 		r-=1
+		# Now start at second row (this time check for subtrees)
 		while r >= 0:
 			move = 1 # number of spaces to backtrack left
 
 			while move < c:
 
-				current = "current[" + str(r) + "," + str(c) + "]"
-				left_cell = "left[" + str(r) + "," + str(move) + "]"
-				down_cell = "down[" + str(move) + "," + str(c) + "]"
-
+				# current = "current[" + str(r) + "," + str(c) + "]"
+				# left_cell = "left[" + str(r) + "," + str(move) + "]"
+				# down_cell = "down[" + str(move) + "," + str(c) + "]"
 				down_symbs = ""
 				left_symbs = ""
 				addNew = False
-				if neo[r][move] != [None] and neo[move][c] != [None]:
-					for left_sub in neo[r][move]:
-						left_symbs += left_sub[0] + "|"
+				if neo[r][move] != [None] and neo[move][c] != [None]: # Don't use the unfilled parts of the matrix (outside of the triangle).
+					for left_sub in neo[r][move]: # Each possible subtree for this cell.
+						# left_symbs += left_sub[0] + "|"
 
-						for right_sub in neo[move][c]:
-							down_symbs += right_sub[0] + "|"
+						for right_sub in neo[move][c]: # Each possible subtree for this cell.
+							# down_symbs += right_sub[0] + "|"
 
-							sub = left_sub[0] + " " + right_sub[0]
-							if sub in nonTerm:
+							sub = left_sub[0] + " " + right_sub[0] # Represent the combination of both subtrees/nonterminals (possible expansion)
+							if sub in nonTerm: # Is there a rule for this expansion?
 								symbols = nonTermInd[nonTerm[sub]]
-								for symbol in symbols:
+								for symbol in symbols: # What are all the possible left hand sides of the right hand side found for the expansion?
 									pathStr = "(" + symbol + " " + left_sub[1] + " " + right_sub[1] + ")"
 									if neo[r][c] == [None]:
 										neo[r][c] = [[symbol, pathStr]]
